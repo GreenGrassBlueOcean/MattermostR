d242106c6545cf2cfdba267f94e92947dce36064
GreenGrassBlueOcean <43173895+GreenGrassBlueOcean@users.noreply.github.com>
2024-10-02 14:51:47 +0000

    Deploying to gh-pages from @ GreenGrassBlueOcean/MattermostR@2a3e86e4111463ba2fe703716a277dbe6c6e5b8f 🚀

---
+++ reference/figures/HexLogoGen.R
@@ -0,0 +1,9 @@
+library(hexSticker)
+sticker("inst/figures/mattermost.png"
+        , package="MattermostR"
+        , p_size = 20
+        , p_color = "#000000"
+        , s_x=1, s_y=.75, s_width=.6
+        , h_fill	= "#ffffff"
+        , h_color	= "#9CF312"
+        , filename="inst/figures/MattermostR_hex.png")
